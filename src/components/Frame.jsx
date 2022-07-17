45c5d124fc8449183c278bc333c909e1f9eaa998
unknown <yairdaboul@gmail.com>
2022-07-17 15:03:11 +0300

    deploy

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -1,13 +1,14 @@
 import React, {useState} from 'react';
 
-const frames = [
-'Cute+bear.png',
-'Gold+Brush.png',
-'Magical.png',
-'Olive+Branch.png',
-'Ring+of+Fire.png',
-'The+Movies.png'
-]
+import img1 from '../assets/frames/Cute+bear.png'
+import img2 from '../assets/frames/Gold+Brush.png'
+import img3 from '../assets/frames/Magical.png'
+import img4 from '../assets/frames/Olive+Branch.png'
+import img5 from '../assets/frames/Ring+of+Fire.png'
+import img6 from '../assets/frames/The+Movies.png'
+
+
+const frames = [img1, img2, img3, img4, img5, img6];
 
 
 function Frame(props) {
@@ -15,8 +16,8 @@ function Frame(props) {
   return (<div className="row">
 
             {frames.map(frame => {
-              return <div className="col" onClick={()=>{props.setFrame('../assets/frames/'+frame)}}>
-                <img class-Name="frame" src={`../assets/frames/${frame}`} alt="Avatar" />
+              return <div className="col" onClick={()=>{props.setFrame(frame)}}>
+                <img className="frame" src={frame} alt="Avatar" />
               </div>
             })}
 
